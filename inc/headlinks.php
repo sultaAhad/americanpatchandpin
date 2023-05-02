d252dab1fe611746be7e7f388b0ac19a98cc8581
sultaAhad <101328284+sultaAhad@users.noreply.github.com>
2023-05-02 20:17:55 +0500

    first commit

---
+++ inc/headlinks.php
@@ -0,0 +1,34 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Site Title -->
+<title>American Patch & Pin </title>
+<!-- Site Title -->
+
+
+<!-- Favicon -->
+<link rel="icon" type="img/favicon.webp" href="img/app-logo.png">
+<!-- Favicon -->
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Bootstrap CSS -->
+
+<!-- Font Awesome CDN -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Font Awesome CDN -->
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+<!-- Anime.js CDN -->
+<link rel="stylesheet" href="css/animate.css">
+<!-- Anime.js CDN -->
+
+<!-- Css Stylesheet -->
+<link rel="stylesheet" href="css/style.css">
+<!-- Css Stylesheet -->
+
+<!-- slick slider cdn -->
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css">
+<!-- slick slider cdn -->
---
+++ index.php
@@ -0,0 +1,132 @@
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/variable.css">
+
+<!-- App Include Here -->
+<?php include 'inc/app.php'; ?>
+<!-- App Include Here -->
+
+<section class="form-wrapper">
+    <div class="container">
+        <div class="col-lg-10 mx-auto">
+            <div class="heading-image">
+                <img src="img/app-logo.png" class="img-fluid" alt="">
+            </div>
+            <div class="form-heading">
+                <h1 class="ml3">Website under Maintenance</h1>
+                <p>Please email <a href=""> sales@americanpatchandpin.com </a> with any quotes, or general questions that you may have.
+                </p>
+            </div>
+            <div class="form-wrapper-main">
+                <form action="">
+                    <div class="row">
+                        <div class="col-lg-6 p-0">
+                            <div class="form-outline">
+                                <input type="text" name="first_name" placeholder="First Name" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 p-0">
+                            <div class="form-outline">
+                                <input type="text" name="first_name" placeholder="Last Name" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 p-0">
+                            <div class="form-outline">
+                                <input type="number" name="first_name" placeholder="Phone Number" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 p-0">
+                            <div class="form-outline">
+                                <input type="email" name="first_name" placeholder="Email" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 p-0">
+                            <div class="select-control">
+                                <select name="driving_habit[miles_do_you_drive]" class="form-select " aria-label="Default select example">
+                                    <option value="">Select Quantity
+                                    </option>
+                                    <option value="1">6</option>
+                                    <option value="2">12</option>
+                                    <option value="3">25</option>
+                                    <option value="3">50</option>
+                                    <option value="3">100</option>
+                                    <option value="3">200</option>
+                                    <option value="3">300</option>
+                                    <option value="3">500</option>
+                                    <option value="3">1000</option>
+                                    <option value="3">2000</option>
+                                    <option value="3">3000</option>
+                                    <option value="3">5000</option>
+                                    <option value="3">5000+</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 p-0">
+                            <div class="select-control">
+                                <select name="driving_habit[miles_do_you_drive]" class="form-select " aria-label="Default select example">
+                                    <option>Select product
+                                    </option>
+                                    <option value="1">Embroidered Patches</option>
+                                    <option value="2">Lapel Pins</option>
+                                    <option value="3">Trading Pins</option>
+                                    <option value="3">Challenge Coins</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 p-0">
+                            <div class="form-outline">
+                                <input type="size" name="first_name" placeholder="Size" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 p-0">
+                            <div class="form-outline">
+                                <input id="date" name="date" placeholder="Date Needed" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 p-0">
+                            <div class="file-upload">
+                                <div class="file-select">
+                                    <div class="file-select-button" id="fileName">No Choose File</div>
+                                    <div class="file-select-name" id="noFile">No file chosen...</div>
+                                    <input type="file" name="chooseFile" id="chooseFile">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 p-0">
+                            <div class="form-outline">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Write a message to us?"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 p-0">
+                            <div class="btn-wrapper">
+                                <a class="btn" href="">Submit</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="social-icon-wrapper">
+                                <a href=""> <img src="img/facebook.png" class="img-fluid" alt=""></a>
+                                <a href=""> <img src="img/instagram.png" class="img-fluid" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer Include Here -->
+<?php include 'inc/footer.php'; ?>
+<!-- Footer Include Here -->
