b0390a172cb6add817865b737a1db1e0d94a84a1
sultan Ahad <101328284+sultaAhad@users.noreply.github.com>
2023-05-09 22:24:56 +0500

    adit

--- home.php
+++ home.php
@@ -751,83 +751,6 @@
                                             <a href="" target="_blank">
                                                 <div class="content-overlay"></div>
                                                 <img class="content-image img-fluid"
-                                                    src="./img/tabs-image/tabs-image28.png" alt="">
-                                                <div class="content-details fadeIn-left">
-                                                    <h3>
-                                                        Click on any image to enlarge </h3>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="container1">
-                                        <div class="content">
-                                            <a href="" target="_blank">
-                                                <div class="content-overlay"></div>
-                                                <img class="content-image img-fluid"
-                                                    src="./img/tabs-image/tabs-image29.png" alt="">
-                                                <div class="content-details fadeIn-left">
-                                                    <h3>
-                                                        Click on any image to enlarge </h3>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="container1">
-                                        <div class="content">
-                                            <a href="" target="_blank">
-                                                <div class="content-overlay"></div>
-                                                <img class="content-image img-fluid"
-                                                    src="./img/tabs-image/tabs-image30.png" alt="">
-                                                <div class="content-details fadeIn-left">
-                                                    <h3>
-                                                        Click on any image to enlarge </h3>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="container1">
-                                        <div class="content">
-                                            <a href="" target="_blank">
-                                                <div class="content-overlay"></div>
-                                                <img class="content-image img-fluid"
-                                                    src="./img/tabs-image/tabs-image31.png" alt="">
-                                                <div class="content-details fadeIn-left">
-                                                    <h3>
-                                                        Click on any image to enlarge </h3>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="container1">
-                                        <div class="content">
-                                            <a href="" target="_blank">
-                                                <div class="content-overlay"></div>
-                                                <img class="content-image img-fluid"
-                                                    src="./img/tabs-image/tabs-image32.png" alt="">
-                                                <div class="content-details fadeIn-left">
-                                                    <h3>
-                                                        Click on any image to enlarge </h3>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mt-4">
-                                <div class="col">
-                                    <div class="container1">
-                                        <div class="content">
-                                            <a href="" target="_blank">
-                                                <div class="content-overlay"></div>
-                                                <img class="content-image img-fluid"
                                                     src="./img/tabs-image/tabs-image33.png" alt="">
                                                 <div class="content-details fadeIn-left">
                                                     <h3>
@@ -992,26 +915,194 @@
         </div>
     </div>
 </section>
-<section class="who-worked-with">
+<section class="who-worked-with incredible-value">
     <div class="container">
         <div class="row">
             <div class="col-lg-10 mx-auto text-center">
-                <h2>WHO WE'VE WORKED WITH
+                <h2>INCREDIBLE VALUE WITH NO HIDDEN FEES
                 </h2>
                 <div class="row">
                     <div class="col-lg-2 mx-auto">
                         <div class="border-bottom"></div>
                     </div>
                 </div>
-                <p>We are proud to partner with leading organizations across the US to provide affordable custom patches
-                    and pins.
-
+                <p>American Patch & Pin is committed providing top quality pins and patches. We’ve been in business
+                    since 1976, so we’ve been creating and designing custom patches that are affordable for over 4
+                    decades.
                 </p>
             </div>
         </div>
         <div class="row">
             <div class="col-lg-10 mx-auto">
+                <div class="row mt-4">
+                    <div class="col-lg-3 p-0">
+                        <div class="fast-free-main">
+                            <img src="img/value1.png" class="img-fluid" alt="">
+                            <h4>FAST 7-12 DAY DELIVERY GUARANTEED
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 p-0">
+                        <div class="fast-free-main">
+                            <img src="img/value2.png" class="img-fluid" alt="">
+                            <h4>FREE PDF PROOF BEFORE PAYMENT
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 p-0">
+                        <div class="fast-free-main">
+                            <img src="img/value3.png" class="img-fluid" alt="">
+                            <h4>DISCOUNTS FOR ALL NON-PROFITS
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 p-0">
+                        <div class="fast-free-main">
+                            <img src="img/value4.png" class="img-fluid" alt="">
+                            <h4>HIGH QUALITY AT FACTORY DIRECT PRICES
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="main-tabs our-forbidden-custom">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 mx-auto">
+                <div class="row">
+                    <div class="col-lg-10 mx-auto text-center">
+                        <h4>CUSTOM
+                        </h4>
+                        <h2>OUR AFFORDABLE CUSTOM
+                        </h2>
+                        <div class="row">
+                            <div class="col-lg-2 mx-auto">
+                                <div class="border-bottom"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="card-wrapper">
+                            <img src="img/card1.png" class="img-fluid" alt="">
+                            <h4>CUSTOM 5</h4>
+                            <ul class="star-wrapper">
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                            </ul>
+                            <h6>15.00</h6>
+                            <div class="banner">
+                                <a href="#" class='butn butn__new'><span>Add to cart</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card-wrapper">
+                            <img src="img/card2.png" class="img-fluid" alt="">
+                            <h4>CUSTOM 5</h4>
+                            <ul class="star-wrapper">
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                            </ul>
+                            <h6>15.00</h6>
+                            <div class="banner">
+                                <a href="#" class='butn butn__new'><span>Add to cart</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card-wrapper">
+                            <img src="img/card3.png" class="img-fluid" alt="">
+                            <h4>CUSTOM 5</h4>
+                            <ul class="star-wrapper">
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                            </ul>
+                            <h6>15.00</h6>
+                            <div class="banner">
+                                <a href="#" class='butn butn__new'><span>Add to cart</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card-wrapper">
+                            <img src="img/card4.png" class="img-fluid" alt="">
+                            <h4>CUSTOM 5</h4>
+                            <ul class="star-wrapper">
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                                <li><i class="fa fa-star-o" aria-hidden="true"></i>
+                                </li>
+                            </ul>
+                            <h6>15.00</h6>
+                            <div class="banner">
+                                <a href="#" class='butn butn__new'><span>Add to cart</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-lg-12 text-center">
+                        <div class="pagination-wrapper">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <li class="page-item">
+                                        <a class="page-link " href="javascript:void(0)#" aria-label="Previous">
+                                            <span aria-hidden="true"><i class="fa fa-long-arrow-left"
+                                                    aria-hidden="true"></i>
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link active" href="javascript:void(0)">1</a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="javascript:void(0)">2</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="javascript:void(0)" aria-label="Next">
+                                            <span aria-hidden="true"><i class="fa fa-long-arrow-right"
+                                                    aria-hidden="true"></i>
 
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
